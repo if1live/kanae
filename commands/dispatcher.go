e2221625cd34b85e128949b36b0ad7b1825b930f
if1live <libsora25@gmail.com>
2017-06-25 13:08:27 +0900

    short comand

--- commands/dispatcher.go
+++ commands/dispatcher.go
@@ -17,7 +17,7 @@ var port int
 var host string
 
 func init() {
-	flag.StringVar(&command, "command", "", "command, use help command to see detail")
+	flag.StringVar(&command, "cmd", "", "command, use help command to see detail")
 	flag.IntVar(&port, "port", 8000, "port to use")
 	flag.StringVar(&host, "host", "127.0.0.1", "address to use")
 }
@@ -29,10 +29,10 @@ type Dispatcher struct {
 
 func NewDispatcher(s kanaelib.Settings) Dispatcher {
 	cmds := map[string]Command{
-		"server": NewServer(s),
-		"sync":   NewSync(s),
-		"dev":    NewDev(s),
-		"help":   NewHelp(),
+		"svr":  NewServer(s),
+		"sync": NewSync(s),
+		"dev":  NewDev(s),
+		"help": NewHelp(),
 	}
 	return Dispatcher{
 		settings: s,
